71152141c689a085c45d0004c61c0bdcfe2b1011
Emilia Saberski <emilia@saberski.com>
2023-05-22 14:43:26 +0200

    Done 8

--- src/8-hacker-speak.js
+++ src/8-hacker-speak.js
@@ -1,3 +1,11 @@
 export const hackerSpeak = (words) => {
-  return words
+let newString = words
+.replaceAll(/a/gi, "4")
+.replaceAll(/e/gi, "3")
+.replaceAll(/i/gi, "1")
+.replaceAll(/o/gi, "0")
+.replaceAll(/s/gi, "5")
+
+  return newString
 }
+console.log(hackerSpeak("javascript is cool"))
