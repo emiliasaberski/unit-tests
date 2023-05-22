dc1efa0a4dcdb84cb27e10d721d6db346fb77e85
Emilia Saberski <emilia@saberski.com>
2023-05-22 14:18:46 +0200

    Done 6-7

--- src/6-filter-numbers.js
+++ src/6-filter-numbers.js
@@ -1,3 +1,12 @@
 export const filterNumbers = (array, largerThan) => {
-  return array
+  const filteredArray = []
+  for (let i = 0; i < array.length; i++) {
+
+  if (array[i] <= largerThan) {
+     filteredArray.push(array[i])
+  }
 }
+return filteredArray
+}
+
+console.log(filterNumbers[1, 20, 25, 34, 99], 25)
